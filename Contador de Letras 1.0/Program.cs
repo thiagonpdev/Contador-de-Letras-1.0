b34c8e3e3156082d8561901904b77967819571d2
Thiago Nogueira <164404698+thiagonpdev@users.noreply.github.com>
2024-09-09 17:04:01 -0300

    Adicionar arquivos de projeto.

---
+++ Contador de Letras 1.0/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Contador_de_letras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linha = string.Empty;
+
+            Console.WriteLine("Digite uma palavra ou texto: ");
+            string texto = Console.ReadLine()!;
+            Console.WriteLine(linha);
+            
+            Console.WriteLine("Digite a letra que deseja contar: ");
+            char letraProcurada = Console.ReadLine()![0];
+            Console.WriteLine(linha);
+
+            texto = texto.ToLower();
+            letraProcurada = char.ToLower(letraProcurada);
+
+            int quantidadeDeLetras = texto.Count(c => c == letraProcurada);
+
+            Console.WriteLine("A letra '" + letraProcurada + "' aparece " + quantidadeDeLetras + " vezes no texto.");
+        }
+    }
+}
